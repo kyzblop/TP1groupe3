ec190513eacd9c2c7411e252218aa2c351afd883
Jerem <Jerem@DESKTOP-8SBULL7>
2023-03-07 12:32:33 +0100

    Creation classe

--- src/main/java/com/inti/model/Vol.java
+++ src/main/java/com/inti/model/Vol.java
@@ -1,5 +1,15 @@
 package com.inti.model;
 
-public class Vol {
+import java.time.LocalDate;
+import java.util.List;
 
+public class Vol {
+	private LocalDate dateDepart;
+	private int heureDepart;
+	private LocalDate dateArrivee;
+	private int heureArrivee;
+	private CompagnieAerienne compagnie;
+	private Aeoroport aeoroportDepart;
+	private Aeoroport aeoroportArrivee;
+	private List<InfosEscale> listEscales;
 }
